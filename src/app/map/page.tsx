631edb4d4dd63708b1efa42da876746e70625588
Jitesh Kumar <jiteshks333@gmail.com>
2025-09-02 12:31:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useLanguage } from '@/context/language-provider';
